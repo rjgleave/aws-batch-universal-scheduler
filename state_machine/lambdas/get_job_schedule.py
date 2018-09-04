b60df39eff71ddf6f94d307433d04e9800fd699c
“Rob <rjgleave@amazon.com>
2018-09-04 09:09:42 -0600

    cleanup

--- state_machine/lambdas/get_job_schedule.py
+++ state_machine/lambdas/get_job_schedule.py
@@ -29,45 +29,6 @@ REGION = os.environ['AWS_REGION']
 dynamodb = boto3.resource("dynamodb", region_name=REGION)
 table = dynamodb.Table('BatchSchedules')
 
-# This indicates where all of the batch job schedules are stored, for
-
-# table contains job schedules. Each must be a json schedule record like this:
-
-'''  Test file for Lambda
-{    
-  "scheduleId" : "daily-pm-load"
-}
-'''
-
-
-'''   SAMPLE JOB SCHEDULE DOCUMENT - TO BE RETRIEVED FROM DYNAMODB
-{    
-    "scheduleId" : "daily-am-load",
-    "category" : "big-data-schedules",
-    "scheduleName" : "Morning load and ETL workflow for big data",
-    "jobList": [
-        {
-            "jobName": "job1",
-            "jobDefinition": "arn:aws:batch:us-east-1:786247309603:job-definition/SampleJobDefinition-49e0468e4a867f5:1",
-            "jobQueue": "arn:aws:batch:us-east-1:786247309603:job-queue/SampleJobQueue-5da08f800c56cd4",
-            "jobDependencies": [],
-            "jobStatus": "",
-            "jobId": ""
-        },
-        {
-            "jobName": "job2",
-            "jobDefinition": "arn:aws:batch:us-east-1:786247309603:job-definition/SampleJobDefinition-49e0468e4a867f5:1",
-            "jobQueue": "arn:aws:batch:us-east-1:786247309603:job-queue/SampleJobQueue-5da08f800c56cd4",
-            "jobDependencies": ["job1"],
-            "jobStatus": "",
-            "jobId": ""
-        }
-    ],
-    "scheduleStatus": "",
-    "wait_time": 60
-}
-'''
-
 
 def lambda_handler(event, context):
     # Log the received event
@@ -91,5 +52,4 @@ def lambda_handler(event, context):
         print("GetItem succeeded:")
         print(json.dumps(item, indent=4, cls=DecimalEncoder))
 
-        #event['scheduleStatus'] = "SUCCEEDED"
         return item
